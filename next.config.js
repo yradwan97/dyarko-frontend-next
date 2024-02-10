74471d7c53859a607fee3646a3be10f5127cc9f1
yradwan97 <youssefer1997@gmail.com>
2024-02-10 18:21:58 +0200

    updated next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-  })
 const nextConfig = {
-    output: "export",
+    
     images: {
         formats: ['image/avif', 'image/webp'],
         remotePatterns: [
