a1c15cd17c7c85d7556568127bbaed77a5168c12
yradwan97 <youssefer1997@gmail.com>
2024-02-11 13:07:27 +0200

    added auth to needed routes

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,12 @@ export {default} from "next-auth/middleware"
 export const config = { 
     matcher:  [
         '/user', 
-        // '/categories', 
-        // '/login/confirm', 
-        // '/videos/:path*', 
-        // '/application/:path*', 
-        // '/property-details/:path*',
-        // '/login/confirm',
-        // '/payment/:path*'
+        '/categories', 
+        '/login/confirm', 
+        '/videos/:path*', 
+        '/application/:path*', 
+        '/property-details/:path*',
+        '/login/confirm',
+        '/payment/:path*'
     ]
 }
