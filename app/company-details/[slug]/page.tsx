02b44b5a4da0f89575e3f7f40826bdc2af856e2c
yradwan97 <youssefer1997@gmail.com>
2024-02-11 12:19:48 +0200

    final fix in company-details

--- app/company-details/[slug]/page.tsx
+++ app/company-details/[slug]/page.tsx
@@ -5,15 +5,6 @@ import CompanyDetailsContent from "../components/CompanyDetailsContent"
 import Header from '@/app/components/Shared/Header/Header';
 import Footer from '@/app/components/Shared/Footer/Footer';
 
-const baseUrl = process.env.NEXT_PUBLIC_NEXT_APP_API_URI
-
-export async function generateStaticParams() {
-  let companies = await fetch(`${baseUrl}/owners`).then(res => res.json())
-  return companies?.data.map((c: any) => {
-    slug: c.slug
-  })
-}
-
 const CompanyDetails = () => {
   const { slug } = useParams();
   return (
