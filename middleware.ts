ed4c463cea1e43d821a921e79e544409a2353572
yradwan97 <youssefer1997@gmail.com>
2024-02-11 12:09:36 +0200

    auth only on /user

--- middleware.ts
+++ middleware.ts
@@ -1,2 +1,14 @@
 export {default} from "next-auth/middleware"
 
+export const config = { 
+    matcher:  [
+        '/user', 
+        // '/categories', 
+        // '/login/confirm', 
+        // '/videos/:path*', 
+        // '/application/:path*', 
+        // '/property-details/:path*',
+        // '/login/confirm',
+        // '/payment/:path*'
+    ]
+}
